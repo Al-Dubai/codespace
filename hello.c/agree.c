6b86494ee80784fef601c96074c96645411252fb
Al-Dubai <126902992+Al-Dubai@users.noreply.github.com>
2023-03-04 14:38:04 +0000

    Sat, Mar 4, 2023, 2:38 PM +00:00

---
+++ hello.c/agree.c
@@ -0,0 +1,11 @@
+#include <cs50.c>
+#include <stdio.c>
+
+int main(void)
+{
+    // prtompt user to agree
+    char c = get_char("do you agree? ");
+
+    // check wether agree
+    if (C == 'Y' || c == 'y')
+}
