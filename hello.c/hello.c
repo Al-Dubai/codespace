b35fc7270f0b9439939155dd0d027fbf2338c0d1
Al-Dubai <126902992+Al-Dubai@users.noreply.github.com>
2023-03-04 00:08:54 +0000

    Sat, Mar 4, 2023, 12:08 AM +00:00

--- hello.c/hello.c
+++ hello.c/hello.c
@@ -2,11 +2,6 @@
 #include <stdio.h>
 int main(void)
 {
-     int x = get_int("what's x? ");
-     int y = get_int("what's y?");
-
-     if (x < y)
-     {
-          printf("x is less than y\n");
-     }
+     string answer = get_string("What's your name? ");
+     printf("hello, %s\n", answer);
 }
